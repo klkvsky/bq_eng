89de201c24641fe9cf12847bc0adf2be1258322a
klkvsky <klkvsky@gmail.com>
2024-10-18 00:00:49 +0500

    [x] Студия на планшете

--- app/(main)/studio/page.tsx
+++ app/(main)/studio/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
     <div className="flex flex-col">
       <PageTitle title={data.heroDescription} categories={[]} />
       <div className="flex flex-col items-end mt-[32px] md:mt-[24px] xl:mt-[80px]">
-        <div className="custom-shadow-left md:after:hidden xl:after:block relative w-[calc(7*12.5vw)] md:w-full xl:w-[calc(9*8.33vw)] h-auto bg-red-500">
+        <div className="custom-shadow-left md:after:hidden xl:after:block relative w-[calc(7*12.5vw)] md:w-full xl:w-[calc(9*8.33vw)] h-auto">
           <Image
             src={urlFor(data.heroImage.asset.url).url()}
             alt="project-1"
@@ -38,9 +38,9 @@ export default async function Home() {
           {data.heroDescription}
         </p>
 
-        <div className="flex flex-col md:flex-row mt-[32px] md:mt-[24px] xl:mt-[80px] px-2 md:px-3 xl:pr-[calc(0.5*8.33vw)] xl:pl-0 gap-[16px] md:gap-[calc(0.5*8.33vw)] w-full xl:w-[calc(9*8.33vw)]">
+        <div className="flex flex-col md:flex-row mt-[32px] md:mt-[24px] xl:mt-[80px] px-2 md:px-3 xl:pr-[calc(0.5*8.33vw)] xl:pl-0 gap-[16px] md:gap-[calc(1.85*8.33vw)] w-full xl:w-[calc(9*8.33vw)] xl:gap-[calc(0.5*8.33vw)]">
           <div
-            className="flex flex-col max-sm:gap-[16px] w-full md:w-[calc(5*8.33vw)] xl:w-[calc(4*8.33vw)]"
+            className="flex flex-col max-sm:gap-[16px] w-full md:w-[calc(4*8.33vw)] xl:w-[calc(4*8.33vw)] "
             id="positions"
           >
             <p className="font-apercu font-normal text-[14px] xl:text-[16px] leading-[20px] -tracking-[0.28px] xl:-tracking-[0.02em] max-sm:mt-[8px]">
@@ -61,7 +61,7 @@ export default async function Home() {
               </div>
             ))}
           </div>
-          <div className="flex flex-col w-full md:w-[calc(5*8.33vw)] xl:w-[calc(4*8.33vw)]">
+          <div className="flex flex-col w-full md:w-[calc(5*8.33vw)] xl:w-[calc(4*8.33vw)] ">
             <p className="font-apercu font-normal text-[14px] xl:text-[16px] leading-[20px] xl:leading-[20px] -tracking-[0.28px] xl:-tracking-[0.02em]">
               {data.workDirections.title}
             </p>
@@ -102,8 +102,8 @@ export default async function Home() {
       <h1 className="font-spectral font-normal text-[20px] md:text-[28px] xl:text-[38px] leading-[24px] md:leading-[32px] xl:leading-[42px] -tracking-[0.6px] md:-tracking-[0.84px] xl:-tracking-[0.03em] px-2 md:px-3 mt-4 md:mt-6">
         {data.bottomDescription}
       </h1>
-      <div className="flex flex-col md:flex-row justify-between mt-[32px] md:mt-20 xl:mt-[80px] gap-4 xl:gap-0 px-2 md:px-3 xl:pl-3 xl:pr-[calc(0.5*8.33vw)]">
-        <div className="flex flex-col gap-4 md:gap-6 xl:gap-10 w-full md:w-[calc(5*8.33vw)] xl:w-[calc(4*8.33vw)]">
+      <div className="flex flex-col md:flex-row xl:justify-between mt-[32px] md:mt-20 xl:mt-[80px] gap-4 xl:gap-0 px-2 md:px-3 xl:pl-3 xl:pr-[calc(0.5*8.33vw)]  md:gap-[calc(0.85*8.33vw)]">
+        <div className="flex flex-col gap-4 md:gap-6 xl:gap-10 w-full md:w-[calc(5*8.33vw)] xl:w-[calc(4*8.33vw)] ">
           <div className="flex flex-col gap-3 w-full md:w-[calc(5*8.33vw)] xl:w-[calc(6*8.33vw)]">
             <p className="font-apercu font-normal text-[14px] xl:text-[16px] leading-[20px] xl:leading-[20px] -tracking-[0.02em]">
               {data.partnershipSection.title}
@@ -147,7 +147,7 @@ export default async function Home() {
             </div>
           )}
         </div>
-        <div className="flex flex-col gap-4 md:gap-6 xl:gap-10 w-full md:w-[calc(5*8.33vw)] xl:w-[calc(4*8.33vw)]">
+        <div className="flex flex-col gap-4 md:gap-6 xl:gap-10 w-full md:w-[calc(5*8.33vw)] xl:w-[calc(4*8.33vw)] ">
           <div className="flex flex-col gap-3">
             <p className="font-apercu font-normal text-[14px] xl:text-[16px] leading-[20px] xl:leading-[20px] -tracking-[0.02em]">
               {data.clientsSection.title}
