b3f9eb1daf8eaa40912873ae9a0b71cc35bd6944
klkvsky <hello@vosk.design>
2024-10-18 18:08:47 +0500

    [x] 4k Fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
       md: "768px",
       lg: "1024px",
       xl: "1280px",
-      "2xl": "2048px", // Changed from the default 1536px to 2048px (2K)
+      "2xl": "3840px",
     },
   },
   plugins: [],
